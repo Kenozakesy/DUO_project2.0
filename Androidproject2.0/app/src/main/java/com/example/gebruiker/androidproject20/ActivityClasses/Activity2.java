c6c9da0562374fa18f6334667183bfbeed02aa4e
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-03-24 13:19:53 +0100

    commit

---
+++ Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/Activity2.java
@@ -0,0 +1,15 @@
+package com.example.gebruiker.androidproject20.ActivityClasses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gebruiker.androidproject20.R;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
--- Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/MainActivity.java
+++ Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.gebruiker.androidproject20;
+package com.example.gebruiker.androidproject20.ActivityClasses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.gebruiker.androidproject20.R;
 
 public class MainActivity extends AppCompatActivity {
 
