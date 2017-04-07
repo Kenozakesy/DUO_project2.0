2e508f835faf723bc0fe77ae41fda4ea1e2c0adf
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-04-07 13:02:27 +0200

    ffd

--- Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/MainActivity.java
+++ Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.gebruiker.androidproject20.ActivityClasses;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.ViewPropertyAnimatorCompatSet;
 import android.view.View;
 
 import com.example.gebruiker.androidproject20.Classes.Game;
