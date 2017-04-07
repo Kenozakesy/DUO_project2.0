5280de0bcd4b4e63dfd0ed1c13242b7210509544
cacsomers <caspersomers@live.nl>
2017-04-07 14:03:16 +0200

    hoofdschermen zijn af, cardview niet

--- Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/MainActivity.java
+++ Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.gebruiker.androidproject20.ActivityClasses;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import com.example.gebruiker.androidproject20.Classes.Game;
 import com.example.gebruiker.androidproject20.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
     }
