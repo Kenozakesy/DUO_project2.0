6942dd040b64187b3b3b326801f223e1f296ed29
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-04-07 13:04:13 +0200

    name fix

--- Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/ActivityOptions.java
+++ Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/ActivityOptions.java
@@ -10,6 +10,6 @@ public class ActivityOptions extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_option);
+        setContentView(R.layout.activity_options);
     }
 }
