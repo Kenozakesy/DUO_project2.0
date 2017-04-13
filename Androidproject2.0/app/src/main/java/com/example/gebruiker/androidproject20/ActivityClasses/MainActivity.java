a6c336ab49f15f63d19ecb9f47571d950b34080c
cacsomers <caspersomers@live.nl>
2017-04-13 08:57:40 +0200

    wgr

--- Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/MainActivity.java
+++ Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.Window;
 
@@ -20,7 +18,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 
