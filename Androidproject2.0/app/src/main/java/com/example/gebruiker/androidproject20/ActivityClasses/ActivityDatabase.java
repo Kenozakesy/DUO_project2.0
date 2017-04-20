f511990a77ee132d2b87c21c6c5cb936bfac3c9e
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-04-20 14:51:04 +0200

    good luck

--- Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/ActivityDatabase.java
+++ Androidproject2.0/app/src/main/java/com/example/gebruiker/androidproject20/ActivityClasses/ActivityDatabase.java
@@ -1,8 +1,11 @@
 package com.example.gebruiker.androidproject20.ActivityClasses;
 
+import android.icu.util.Output;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
+import android.widget.TextView;
 
 import com.example.gebruiker.androidproject20.R;
 import com.google.firebase.database.DataSnapshot;
@@ -15,24 +18,42 @@ import java.util.jar.Attributes;
 
 public class ActivityDatabase extends AppCompatActivity
 {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView text = (TextView) findViewById(R.id.tvDatabase);
+
+    //private Firebase Mybase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_database);
+
         GetData();
     }
 
-    public void GetData()
-    {
+    //does not work for some reason (cannot make the data show up anymore)
+    public void GetData() {
+
         // Get a reference to our posts
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("Players/Player1/Name");
 
-        String name = ref.getRef().toString();
+        ref.addValueEventListener(new ValueEventListener() {
 
-     
 
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String value = dataSnapshot.getValue(String.class);
+
+                text.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
